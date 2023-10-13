6b3c49d369b20ee20b353841b212ff2807b4c8dd
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2023-10-13 14:15:09 +0700

    Add options and arguments to CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,10 @@ import { Command } from 'commander';
 
 const program = new Command();
 
-program.description('Compares two configuration files and shows a difference.').version('1.0.0');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
